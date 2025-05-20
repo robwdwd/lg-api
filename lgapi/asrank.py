b919288aefe89c93e87e7b15229f06678a1594af
Rob Woodward <rob@emailplus.org>
2025-05-20 16:03:44 +0100

    Removed unused function

--- lgapi/asrank.py
+++ lgapi/asrank.py
@@ -8,27 +8,6 @@
 import httpx
 
 
-async def api(url: str) -> dict:
-    """Performs an asynchronous HTTP GET request to the specified URL using an AsyncClient.
-    Handles HTTP errors and returns the JSON response data if successful.
-
-    Args:
-        url (str): The URL to fetch data from.
-
-    Returns:
-        dict: The JSON response data, or an empty dictionary if there is an error or the data is not present.
-    """
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url)
-            response.raise_for_status()
-            result = response.json()
-        except httpx.HTTPError:
-            return {}
-
-    return {} if "error" in result or "data" not in result else result["data"]
-
-
 def AsnQuery(asn: int):
     """Format the GraphQL content for retriving the data."""
     return """{
@@ -54,15 +33,10 @@ async def asn_to_name(asn: int) -> dict:
     async with httpx.AsyncClient() as client:
         try:
             query = AsnQuery(asn)
-            response = await client.post(
-                "https://api.asrank.caida.org/v2/graphql",
-                json={'query': query}
-                )
+            response = await client.post("https://api.asrank.caida.org/v2/graphql", json={"query": query})
             response.raise_for_status()
             result = response.json()
         except httpx.HTTPError:
             return {}
 
-    return {} if "error" in result or "data" not in result or "asn" not in result['data'] else result["data"]['asn']
-
-
+    return {} if "error" in result or "data" not in result or "asn" not in result["data"] else result["data"]["asn"]
